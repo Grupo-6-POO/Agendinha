082844fb49944bb26d78008b31cbb130a6c0603f
Lucas Haubmann <lucas.a.haubmann@gmail.com>
2024-10-31 18:46:49 -0300

    BRUNA - Criação método para salvar dados em arquivos txt

--- src/application/Aplication.java
+++ src/application/Aplication.java
@@ -1,7 +1,30 @@
 package application;
-
-import entities.Category;
+import entities.User;
+import java.io.*;
+import java.util.ArrayList;
 
 //Classe teste
-public class Aplication {
-  }
+public class Aplication implements Serializable{
+    public static void main(String[] args) throws IOException,
+          ClassNotFoundException {
+      ArrayList <User> usuarios = new ArrayList<>();
+      usuarios.add(new User("Bruna", "1234"));
+      usuarios.add(new User("Lucas", "5678"));
+      for(User usuario : usuarios){
+        salvarUsuario(usuario);}
+      System.out.println(usuarios);
+
+    }
+
+
+      public static void salvarUsuario( User usuario) throws IOException {
+        FileOutputStream fos = new FileOutputStream("../data.txt");
+        ObjectOutputStream os = new ObjectOutputStream(fos);
+
+        os.writeObject(usuario);
+        os.close();
+        fos.close();
+      }
+
+}
+
