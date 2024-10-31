26de98159f61e7eba97430c34b70629312f4c20d
Lucas Haubmann <lucas.a.haubmann@gmail.com>
2024-10-31 09:27:20 -0300

    Criação/Manutenção classe ManagerException

--- src/exceptions/ManagerException.java
+++ src/exceptions/ManagerException.java
@@ -2,7 +2,8 @@ package exceptions;
 
 public class ManagerException extends RuntimeException{
 
-    public ManagerException(String message){
-        super(message);
-    }
+        public ManagerException(String message) {
+            super(message);
+       }
 }
+
